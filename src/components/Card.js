2b1d130f935ef70ee2132e338a34f32ce4287e9d
vishal-koundal <vishal2661999@gmail.com>
2019-08-08 05:04:10 -0700

    minor fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Button, Title, Subtitle } from './elements';
 
-const Container = styled.div`
-  .cards {
-    background: transparent;
-  }
-  .media-content {
-    margin-top: 1.5rem;
-  }
-`;
-
 const Card = ({ title, subtitle, button, Icon }) => (
-  <Container>
-    <div className="cards">
-      <div className="column">
-        <img src={Icon} />
-      </div>
-      <div className="media-content has-text-centered">
-        <Title red>{title}</Title>
-        <Subtitle>{subtitle}</Subtitle>
-        <Button borderRed className="has-text-weight-semibold">
-          {button}
-        </Button>
-      </div>
+  <div>
+    <div>
+      <img src={Icon} />
     </div>
-  </Container>
+    <div className="media-content has-text-centered">
+      <Title red>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+      <Button borderRed className="has-text-weight-semibold">
+        {button}
+      </Button>
+    </div>
+  </div>
 );
 
 export default Card;
