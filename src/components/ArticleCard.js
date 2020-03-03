3f93bf83c25177a013ffbd8e2ff1f9ff75dbe319
karanbir8872 <wwwkaranbir786@gmail.com>
2020-03-03 16:07:42 +0530

    Minor fixes

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -5,7 +5,7 @@ const Container = styled.div`
   .card {
     border-radius: 1rem;
     background-color: ${props => props.theme.darkShades};
-    height: 36rem;
+    height: 40rem;
   }
   .title {
   }
@@ -31,9 +31,9 @@ const ArticleCard = ({ image, alt, h2, title, subtitle }) => (
         </figure>
       </div>
       <div className="media-content has-text-centered">
-        <h2 className="subtitle is-6 has-text-danger">{h2}</h2>
-        <h2 className="title is-6  has-text-white is-spaced">{title}</h2>
-        <p className="subtitle is-7 has-text-white">{subtitle}</p>
+        <h2 className="subtitle is-6 has-text-danger is-spaced">{h2}</h2>
+        <h2 className="title is-5  has-text-white is-spaced">{title}</h2>
+        <p className="subtitle is-6 has-text-white">{subtitle}</p>
         <button
           className="button is-danger is-rounded has-text-weight-bold"
           type="Read More"
