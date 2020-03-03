c986a6f1aa861217e251e151e611ee9030d85ff0
karanbir8872 <wwwkaranbir786@gmail.com>
2020-03-03 12:23:13 +0530

    minor fixes in BlogProject

--- src/components/BlogProjectIdea.js
+++ src/components/BlogProjectIdea.js
@@ -2,19 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Section = styled.section`
+  .card {
+    width: 24rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
   .card-image {
     padding: 1rem 1rem;
     background-color: ${props => props.theme.mainBrandColor};
+    height: 13.5rem;
+  }
+  .card-content {
+    height: 6rem;
   }
 `;
 
 const BlogProjectIdea = () => (
-  <Section className="section">
-    <div className="container">
+  <Section>
+    <div>
       <div className="card has-text-centered">
         <div className="card-image">
-          <h3 className="title is-4 is-spaced">Do you have project in mind?</h3>
-          <h3 className="subtitle is-6 has-text-weight-semibold">
+          <h3 className="title is-3 is-spaced">Do you have project in mind?</h3>
+          <h3 className="subtitle is-5">
             Complete our quick-fill enquiry form today and receive a quote
             within 48 hours
           </h3>
