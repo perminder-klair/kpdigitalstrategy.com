19fd17a8b2b43781ea47dc61f18e030056bf30e6
karanbir8872 <wwwkaranbir786@gmail.com>
2020-03-30 22:02:02 +0530

    add  product schemas

--- schemas/product.js
+++ schemas/product.js
@@ -37,5 +37,67 @@ export default {
       title: 'Hero Subtitle',
       type: 'string',
     },
+    {
+      name: 'ourValue',
+      title: 'Our Value',
+      type: 'array',
+      of: [{ type: 'string' }],
+    },
+    {
+      name: 'strengthTitle',
+      title: 'Strength Title',
+      type: 'string',
+    },
+    {
+      name: 'strengthSubtitle',
+      title: 'Strength Subtitle',
+      type: 'text',
+    },
+    {
+      name: 'features',
+      title: 'Features',
+      type: 'features',
+    },
+    {
+      name: 'reachUsText',
+      title: 'ReachUs Text',
+      type: 'string',
+    },
+    {
+      name: 'portfolio',
+      title: 'Portfolio',
+      type: 'portfolio',
+    },
+    {
+      name: 'brandTitle',
+      title: 'Brand Title',
+      type: 'string',
+    },
+    {
+      name: 'brandIcons',
+      title: 'Brand Icons',
+      type: 'array',
+      of: [{ type: 'image' }],
+    },
+    {
+      name: 'testimonialItem',
+      title: 'Testimonial Item',
+      type: 'testimonialItem',
+    },
+    {
+      name: 'ourGrowthTitle',
+      title: 'Our Growth Title',
+      type: 'string',
+    },
+    {
+      name: 'ourGrowthSubtitle',
+      title: 'Our Growth Subtitle',
+      type: 'string',
+    },
+    {
+      name: 'ourGrowthdescription',
+      title: 'Our Growth Description',
+      type: 'text',
+    },
   ],
 };
