1aa48756337685b4fb40fb991cf79b19ae61d993
karanbir8872 <wwwkaranbir786@gmail.com>
2020-04-09 13:30:18 +0530

    overview page fix

--- src/components/ServiceHero.js
+++ src/components/ServiceHero.js
@@ -13,32 +13,13 @@ const ServiceHero = ({ data }) => (
           {data.map(items => (
             <div className="column is-6">
               <ServiceCard
-                title={items.category}
-                subtitle={items.title}
-                firstpara={items.description}
-                secondpara={items.description}
-                bgImage={items.Thumbnail.asset.url}
+                title={items.node.category}
+                subtitle={items.node.title}
+                firstpara={items.node.description}
+                bgImage={items.node.Thumbnail.asset.url}
               />
             </div>
           ))}
-          {/* <div className="column is-6">
-            <ServiceCard
-              title="Digital marketing"
-              subtitle="Strategy & execution"
-              firstpara="Competitor reseach and insights, strategic planning, goal setting."
-              secondpara="Set up and optimisation of campaigns, on-going analysis & improvement. Digital Ads including Google, Bing & Social. Targeted email newslettes & LinkedIn outreach."
-              bgImage="/images/service/laptop.jpg"
-            />
-          </div>
-          <div className="column is-6">
-            <ServiceCard
-              title="Website design & development"
-              subtitle="Drive engagement & business growth "
-              firstpara="Bespoke built, responsive, secure, pixel-perfect website design and development GATSBY JS coded.  Wordpress. Contentful CMS options."
-              secondpara="Lead generation, landing page creation. Built for business growth."
-              bgImage="/images/service/code.jpg"
-            />
-          </div> */}
         </div>
       </section>
     </div>
--- src/pages/overView.js
+++ src/pages/overView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { graphql } from 'gatsby';
 import config from '../utils/config';
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
@@ -9,42 +9,43 @@ import ServiceHero from '../components/ServiceHero';
 
 export const overViewQuery = graphql`
   query overView {
-    sanityDigitalService {
-      category
-      title
-      description
-      Thumbnail {
-        asset {
-          url
+    allSanityDigitalService {
+      edges {
+        node {
+          slug {
+            current
+          }
+          category
+          title
+          description
+          Thumbnail {
+            asset {
+              url
+            }
+          }
         }
-      }
-      slug {
-        current
       }
     }
   }
 `;
 
-export default class OverViewPage extends React.Component {
-  render() {
-    const {
-      data: { sanityDigitalService: overView },
-    } = this.props;
-    return (
-      <Layout>
-        <Seo
-          title="Manchester Digital Marketing Agency"
-          description="KP Digital Strategy"
-          url={config.siteUrl}
-          image={config.image}
-        />
-        <PageHero
-          title="Our digital marketing services"
-          subtitle="We align design, marketing and sales to make it easier than every to reach new business heights. "
-        />
-        <ServiceHero data={overView} />
-        {/* <OurValue /> */}
-      </Layout>
-    );
-  }
-}
+const OverViewPage = ({ data }) => {
+  const page = data.allSanityDigitalService.edges;
+  return (
+    <Layout>
+      <Seo
+        title="Manchester Digital Marketing Agency"
+        description="KP Digital Strategy"
+        url={config.siteUrl}
+        image={config.image}
+      />
+      <PageHero
+        title="Our digital marketing services"
+        subtitle="We align design, marketing and sales to make it easier than every to reach new business heights. "
+      />
+      <ServiceHero data={page} />
+      {/* <OurValue /> */}
+    </Layout>
+  );
+};
+export default OverViewPage;
