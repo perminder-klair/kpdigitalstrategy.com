5e565a215a54a0ec83ef64356df46dbcafc1fe49
karanbir8872 <wwwkaranbir786@gmail.com>
2020-04-22 19:15:35 +0530

    add data in blog page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -5,15 +5,29 @@ import config from '../utils/config';
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import PageHero from '../components/PageHero';
-// import Faq from '../components/Faq';
+import Faq from '../components/Faq';
 import SearchBar from '../components/SearchBar';
 import FeaturesArticles from '../components/FeaturesArticles';
 // import BrandIdentity from '../components/BrandIdentity';
-// import OurValue from '../components/OurValue';
-// import ReachUs from '../components/ReachUs';
+import OurValue from '../components/OurValue';
+import ReachUs from '../components/ReachUs';
 
 export const blogQuery = graphql`
   query blogQuery {
+    sanitySiteSettings {
+      blogMainTitle
+      blogMainSubtitle
+      blogOurValue
+      blogFaq {
+        faqTitle
+        faqSubtitle
+        faqQuestion {
+          question
+          answer
+        }
+      }
+      blogReachUsText
+    }
     allSanityBlog {
       edges {
         node {
@@ -46,6 +60,9 @@ export default class Blog extends React.Component {
     const {
       data: { allSanityBlog: blog },
     } = this.props;
+    const {
+      data: { sanitySiteSettings: page },
+    } = this.props;
     return (
       <Layout>
         <Seo
@@ -54,16 +71,13 @@ export default class Blog extends React.Component {
           url={config.siteUrl}
           image={config.image}
         />
-        <PageHero
-          title="Our design & marketing advice centre"
-          subtitle="A collection of resources to support your business growth"
-        />
+        <PageHero title={page.blogMainTitle} subtitle={page.blogMainSubtitle} />
         <SearchBar onChange={value => this.setState({ searchQuery: value })} />
         <FeaturesArticles items={blog.edges} filter={searchQuery} />
-        {/* <BrandIdentity />
-        <OurValue />
-        <Faq />
-        <ReachUs /> */}
+        {/* <BrandIdentity /> */}
+        <OurValue data={page.blogOurValue} />
+        <Faq data={page.blogFaq} />
+        <ReachUs data={page} />
       </Layout>
     );
   }
